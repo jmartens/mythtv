f645143834ed168edbe15b67465efe3b64b5c32e
Daniel Kristjansson <dkristjansson@mythtv.org>
2010-10-04 15:43:10 +0000

    Document udev rule & script needed for DVEO ASI devices.
    git-svn-id: http://svn.mythtv.org/svn/branches/mythtv-rec@26633 7dbf422c-18fa-0310-86e9-fd20926502f2

--- mythtv/libs/libmythtv/asirecorder.h
+++ mythtv/libs/libmythtv/asirecorder.h
@@ -21,6 +21,33 @@ class TVRec;
  *  \brief This is a specialization of DTVRecorder used to
  *         handle streams from ASI drivers.
  *
+ *  This has been written and tested with the DVEO ASI hardware and drivers.
+ *  Those particular drivers do not come with udev rules so you will need
+ *  to create the following rule in /etc/udev/rules.d/99-asi.rules
+\verbatim
+SUBSYSTEM=="asi", OWNER="mythtv", GROUP="video", MODE="0660", RUN+="/etc/udev/asi.sh" OPTIONS+="last_rule"
+\endverbatim
+Also, create a /etc/udev/asi.sh file with the following contents:
+\verbatim
+#!/bin/sh
+
+for VAL in buffers bufsize dev granularity mode null_packets timestamps transport uevent clock_source; do
+  sudo chown mythtv /sys/devices/*/*/*/dvbm/*/asi*/$VAL ;
+  sudo chgrp video /sys/devices/*/*/*/dvbm/*/asi*/$VAL ;
+  sudo chmod g+rw /sys/devices/*/*/*/dvbm/*/asi*/$VAL ;
+  sudo chmod g-x /sys/devices/*/*/*/dvbm/*/asi*/$VAL ;
+  sudo ls -l /sys/devices/*/*/*/dvbm/*/asi*/$VAL ;
+done
+\endverbatim
+ *
+ * Be sure to mark asi.sh as executable with "chmod a+x /etc/udev/asi.sh"
+ * udev will silently ignore it if it is not marked.
+ *
+ * This of course assumes you want MythTV to have access to all asi
+ * device, if this is not the case, you will need to write a more
+ * specific rule and modify the asi.sh to only grant permission
+ * on a specific asi revice
+ *
  *  \sa DTVRecorder
  */
 class ASIRecorder : public DTVRecorder
